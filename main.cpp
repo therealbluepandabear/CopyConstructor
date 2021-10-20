80e7e6176c06e82d6f3c32cf048a9369cbb16882
xTheCoderx <justaguyoney@gmail.com>
2021-10-20 18:23:10 +1300

    DO NOT RUN THIS CODE... YOU WILL GET AN EXCEPTION.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include "Integer.h"
 
+void Print(Integer i) {
+
+}
+
+Integer Add(int x, int y) {
+    return Integer(x + y);
+}
+
 int main() {
     Integer i(5);
+    Integer i2(i);
     std::cout << i.GetValue() << std::endl;
     return 0;
 }
