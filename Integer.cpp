7cf3f78363f0761897a2a1dfd435cfa313eaba58
xTheCoderx <justaguyoney@gmail.com>
2021-10-20 18:03:56 +1300

    Initial commit

---
+++ Integer.cpp
@@ -0,0 +1,13 @@
+//
+// Created by TO on 10/20/2021.
+//
+
+#include "Integer.h"
+
+void Integer::SetValue(int value) {
+
+}
+
+int Integer::GetValue() const {
+    return 0;
+}
---
+++ Integer.h
@@ -0,0 +1,11 @@
+//
+// Created by TO on 10/20/2021.
+//
+class Integer {
+    int *m_pInt;
+public:
+    Integer();
+    Integer(int value);
+    int GetValue() const;
+    void SetValue(int value);
+};
